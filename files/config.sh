ba9e8a4a4b8e8ba1442a57b1ff415129d546192d
Thomas <thomasfeldmann@synology.com>
2018-08-15 17:20:57 +0200

    +config.sh

--- files/config.sh
+++ files/config.sh
@@ -9,9 +9,11 @@ if grep -qE "(Microsoft|WSL)" /proc/version &> /dev/null ; then
     subl=/mnt/c/Program\ Files/Sublime\ Text\ 3/subl.exe
     os=win
 else
-    #subl=subl #SublimeText 3
-    subl='tilix -e vim -p' #vim
-    OpenFiles='"$DEBUG_DIR" "$SMART_GREP" "$PACK" "$Bash_history" "$hb_debug" "$HB" "$DF" "$IFCONFIG" "$SPACE_FILES" "$SYSDBtac" "$sm" "$MESSAGES"'
+    subl=subl #SublimeText 3
+    #subl='tilix -e vim -p' #vim
+    declare -a OpenFiles
+    OpenFiles=("${DEBUG_DIR}" "${SMART_GREP}" "${PACK}" "${Bash_history}" "${hb_debug}" "${HB}" "${DF}" "${IFCONFIG}" "${SPACE_FILES}":100000 "${SYSDBtac}":100000 "${sm}" "${MESSAGES}":1000000 "${PicArray[@]}")
     #subl='tilix -e vim -g -p' #vim-gnome
+    #to add:
     os=other
 fi

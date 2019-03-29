00b83bbaf57695173b12b018022131378038bddc
thomas <thomasfeldmann@mail.de>
2019-03-29 13:24:53 +0100

    / changes.

--- files/config.sh
+++ files/config.sh
@@ -19,7 +19,7 @@ else
     #subl='tilix -e vim -p' #vim
     #OpenFiles=("${DEBUG_DIR} ${SMART_GREP} ${PACK} ${Bash_history} ${hb_debug} ${HB} ${sm} ${MESSAGES}") #for vim
     #OpenFiles=("${DEBUG_DIR}" "${SMART_GREP}" "${PACK}" "${Bash_history}" "${hb_debug}" "${HB}" "${DF}" "${IFCONFIG}" "${SPACE_FILES}" "${DiskLog}" "${KERN}" "${SYSDBtac}" "${sm}" "${MESSAGES}" "${PicArray[@]}") # for visualcode
-    OpenFiles=("${DEBUG_DIR}" "${SMART_GREP}" "${PACK}" "${Bash_history}" "${hb_debug}" "${HB}" "${DF}" "${IFCONFIG}" "${SPACE_FILES}":100000 "${DiskLog}" "${KERN}":100000 "${SYSDB}":100000 "${MESSAGES}":1000000  "${sm}" "${PicArray[@]}") # for subl
+    OpenFiles=("${DEBUG_DIR}" "${SMART_GREP}" "${PACK}" "${Bash_history}" "${hb_debug}" "${HB}" "${DF}" "${IFCONFIG}" "${SPACE_FILES}":100000 "${DiskLog}":100000 "${KERN}":100000 "${SYSDB}":100000 "${MESSAGES}":1000000  "${sm}" "${PicArray[@]}") # for subl
     #subl='tilix -e vim -g -p' #vim-gnome
     #to add:
     #IFS=$' \t\n'
